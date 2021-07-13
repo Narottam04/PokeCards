7b1d1604ec061919dea8cd7b16b86b4fe54911e2
Narottam04 <coutkunal@gmail.com>
2021-07-13 19:50:32 +0530

    sw fix try 1

--- src/sw.js
+++ src/sw.js
@@ -275,7 +275,6 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    //console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
             return cacheRes || fetch(evt.request);
